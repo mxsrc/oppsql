72579071f60d1321b78801f11915555a1a0b757b
Max Schettler <max.schettler@posteo.de>
2018-06-06 10:03:14 +0200

    Update readme and documentation

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 # oppsql
 This package provides tooling for interacting with OMNeT++'s SQLite result files.
 It contains a script for merging individual result databases, and a small python library to access the data.
-Note that this repository is currently only contains a few helpers I wrote and is thus only used by me.
-Therefore it, likely contains bugs and is not stable.
+
+Please note that the repository is not stable and its behavior might change without notice.
+
+If you encounter any issues, please report them or open a PR.
+
+## Example use
+
+```
+> mergeDBs -p results
+> python
+>>> import sqlalchemy as sqa
+>>> import oppsql
+>>> engine = sqa.create_engine('sqlite:///results/out.db')
+>>> df = oppsql.get_vector(engine, {'nCars': 320, 'repetition': None}, 'collisions',
+		filter_=oppsql.model.vectordata.c.value > 300, aggregate=sqa.func.avg)
+>>> df.head()
+  repetition  collisions
+0          0  552.576250
+1          1  546.687829
+2          2  549.550911
+3          3  553.535197
+4          4  540.700926
+```
--- oppsql/__init__.py
+++ oppsql/__init__.py
@@ -19,6 +19,13 @@ def _ignore_decimal_warning():
 
 
 def get_unique_param(con, name, type):
+    """Get a param which is unique for the database
+
+    Raises
+    ------
+    sqlalchemy.MultipleResultsFound
+        If the parameter is not unique.
+    """
     return type(con.execute(sqa.select([m.runparam.c.parValue])
                                .where(m.runparam.c.parName.like('%{}'.format(name)))
                                .distinct())
@@ -26,6 +33,89 @@ def get_unique_param(con, name, type):
 
 
 def get_vector(engine, by, variable, time=False, run=False, module=False, filter_=None, aggregate=None):
+    """Get OMNeT++ result vectors
+
+    Parameters
+    ----------
+    engine : sqlalchemy.engine.Engine
+        Database connection.
+    by : str or list or tuple or dict
+        The attribute(s) to group results by. Depending on the type, the semantic changes
+        Passing a string, list or tuple will group by the given attributes (as present in the runattr table).
+        For each attribute, a column is added which contains the corresponding values.
+        When given a dict, its keys are interpreted as a list/tuple. The values can be used to filter the results.
+        If the value is a list/tuple, only rows containing the specified entries will be returned. If the value is
+        a single value, additionally, the corresponding column is dropped.
+    variable : string or list or tuple
+        The variable(s) to query. If multiple values are given, a additional column containing the variable name is
+        present, if only one is given the value column is renamed to the variable name.
+    time : bool
+        Include the values' timestamps (in seconds).
+    module : bool
+        Include the emitting module's name.
+    filter_ : sqlalchemy expression
+        A valid sqlalchemy constraint that is checked agaisnt all returned rows.
+    aggregate : sqlalchemy aggregation function
+        If given, results are grouped by the attributes given in `by` and the aggregated by the given
+        function is applied.
+
+    Notes
+    -----
+    The simulation repetion number can be included by adding 'repetition' to the attributes. If not included,
+    the results of all simulation runs will be included without being able to distinguish them.
+
+    Examples
+    --------
+    For all attribute-values of 'nCars' return all collisions values.
+    >>> df = oppsql.get_vector(engine, 'nCars', 'collisions')
+    >>> df.head()
+      nCars  collisions
+    0   160       150.0
+    1   160       161.0
+    2   160       138.0
+    3   160       151.0
+    4   160       155.0
+
+    Include the repetition information.
+    >>> df.head()
+      nCars repetition  collisions
+    0   160          4       150.0
+    1   160          4       161.0
+    2   160          4       138.0
+    3   160          4       151.0
+    4   160          4       155.0
+
+    Only return results where nCars equals 320.
+    >>> df = oppsql.get_vector(engine, {'nCars': 320, 'repetition': None}, 'collisions')
+    >>> df.head()
+      repetition  collisions
+    0          2       484.0
+    1          2       589.0
+    2          2       585.0
+    3          2       536.0
+    4          2       570.0
+
+    Compute the average collisions for each repetition of nCars == 320.
+    >>> df = oppsql.get_vector(engine, {'nCars': 320, 'repetition': None}, 'collisions', aggregate=sqa.func.avg)
+    >>> df.head()
+      repetition  collisions
+    0          0  314.153409
+    1          1  364.458553
+    2          2  412.747559
+    3          3  467.429722
+    4          4  366.877266
+
+    Compute the average of collisions above 300 for each repetition of nCars == 320.
+    >>> df = oppsql.get_vector(engine, {'nCars': 320, 'repetition': None}, 'collisions',
+                               filter_=oppsql.model.vectordata.c.value > 300, aggregate=sqa.func.avg)
+    >>> df.head()
+      repetition  collisions
+    0          0  552.576250
+    1          1  546.687829
+    2          2  549.550911
+    3          3  553.535197
+    4          4  540.700926
+    """
     _ignore_decimal_warning()
 
     def simtime(simtime_raw, simtime_exponent):
